8d6cd13547426bb07a321452f1e8ec8f96bb9d57
viyiviyi <viyiviyi@outlook.com>
2023-10-16 23:54:15 +0800

    没写完，暂存一下

--- scripts/encrypt_image.py
+++ scripts/encrypt_image.py
@@ -1,22 +1,95 @@
-from preload import EncryptedImage,get_sha256
 
-from modules import shared
+from modules import shared,script_callbacks
+from scripts.core.core import encrypt_image,get_sha256,dencrypt_image
+from PIL import Image,JpegImagePlugin,PngImagePlugin,WebPImagePlugin
+from PIL import Image as PILImage
+from io import BytesIO
+import piexif
+import piexif.helper
+from typing import Optional
+from fastapi import FastAPI
+from gradio import Blocks
+from fastapi import APIRouter, Depends, FastAPI, Request, Response
+
+class EncryptedImage(Image.Image):
+    password = ''
+    __name__ = 'EncryptedImage'
+    
+    @staticmethod
+    def open(fp,*args, **kwargs):
+        image = PILImage.open(fp,*args, **kwargs)
+        encrypt_type = ''
+        print('read*********')
+        if image.format.lower() == PngImagePlugin.PngImageFile.format.lower():
+            pnginfo = image.info or PngImagePlugin.PngInfo()
+            encrypt_type = pnginfo["Encrypt"]
+        elif image.format.lower() in (JpegImagePlugin.JpegImageFile.format.lower(), WebPImagePlugin.WebPImageFile.format.lower()):
+            # exif_dict = piexif.load(image.info["exif"])
+            # encrypt_type = exif_dict["Exif"]['Encrypt']
+            encrypt_type = ''
+            
+        if encrypt_type == 'pixel_shuffle':
+            print('dencrypt: '+str(fp))
+            dencrypt_image(image, EncryptedImage.password)
+        return image
+
+    def save(self, filename, format = None, pnginfo=None, *args, **kwargs):
+        if not EncryptedImage.password:
+            # 如果没有密码，直接保存
+            super().save(filename, format = format, *args, **kwargs)
+        
+        encrypt_image(self, get_sha256(EncryptedImage.password))
+
+        self.format = format or self.format or PngImagePlugin.PngImageFile.format
+        if self.format.lower() == PngImagePlugin.PngImageFile.format.lower():
+            pnginfo = pnginfo or PngImagePlugin.PngInfo()
+            pnginfo.add_text('Encrypt', 'pixel_shuffle')
+            super().save(filename, format=format, pnginfo=pnginfo, *args, **kwargs)
+        elif self.format.lower() in (JpegImagePlugin.JpegImageFile.format.lower(), WebPImagePlugin.WebPImageFile.format.lower()):
+            super().save(filename, format=format, *args, **kwargs)
+            
+        else:
+            super().save(filename, format=format, *args, **kwargs)
+
+Image.Image = EncryptedImage
 
 password = getattr(shared.cmd_opts, 'encrypt_pass', None)
 
-if password:
-    EncryptedImage.set_password(password)
-    section = ("encrypt_image",'图片加密' if shared.opts.localization == 'zh_CN' else "encrypt image" )
-    shared.opts.add_option(
-        "encrypt_pass_hash",
-        shared.OptionInfo(
-            default=get_sha256(password),
-            label='图片解密密码的哈希值, 别改!' if shared.opts.localization == 'zh_CN' else "encrypt image hash password",
-            section=section,
-        ),
-    )
-    shared.opts.data['encrypt_pass_hash'] = get_sha256(password)
+def on_app_started(demo: Optional[Blocks], app: FastAPI):
     
+    @app.middleware("http")
+    async def image_dencrypt(req: Request, call_next):
+        endpoint:str = req.scope.get('path', 'err')
+        if endpoint.startswith('/file='):
+            file_path = endpoint[6:]
+            print(file_path)
+            ex = file_path[file_path.rindex('.'):].lower()
+            if ex in ['.png','.jpg','.jpeg','.webp','.abcd']:
+                image = PILImage.open(file_path)
+                dencrypt_image(image,get_sha256(EncryptedImage.password))
+                buffered = BytesIO()
+                image.save(buffered, format=JpegImagePlugin.JpegImageFile.format)
+                decrypted_image_data = buffered.getvalue()
+                response: Response = Response(content=decrypted_image_data, media_type="image/jpeg")
+                return response
+        res: Response = await call_next(req)
+        return res
+
+script_callbacks.on_app_started(on_app_started)
+    
+
+if password:
     print('图片加密已经启动 加密方式 1')
+    EncryptedImage.password = password
+    # section = ("encrypt_image",'图片加密' if shared.opts.localization == 'zh_CN' else "encrypt image" )
+    # shared.opts.add_option(
+    #     "encrypt_pass_hash",
+    #     shared.OptionInfo(
+    #         default=get_sha256(password),
+    #         label='图片解密密码的哈希值, 别改!' if shared.opts.localization == 'zh_CN' else "encrypt image hash password",
+    #         section=section,
+    #     ),
+    # )
+    # shared.opts.data['encrypt_pass_hash'] = get_sha256(password)
 else:
     print('图片加密插件已安装，但缺少密码参数未启动')
