46117c64908aab97bf86b8690bdd07269998d61e
viyiviyi <viyiviyi@outlook.com>
2023-10-19 17:11:56 +0800

    解决reload ui后导致图片会被重复加密的问题。

--- scripts/encrypt_image.py
+++ scripts/encrypt_image.py
@@ -13,34 +13,39 @@ repo_dir = md_scripts.basedir()
 password = getattr(shared.cmd_opts, 'encrypt_pass', None)
 super_open = PILImage.open
 
-class EncryptedImage(PILImage.Image):
-    def save(self, fp, format=None, **params):
-        if not password:
-            # 如果没有密码，直接保存
-            super().save(fp, format = format, **params)
-            return
-        
-        if 'Encrypt' in self.info and self.info['Encrypt'] == 'pixel_shuffle':
-            super().save(fp, format = format, **params)
-            return
-        
-        encrypt_image(self, get_sha256(password))
-        self.format = PngImagePlugin.PngImageFile.format
-        pnginfo = params.get('pnginfo', PngImagePlugin.PngInfo())
-        pnginfo.add_text('Encrypt', 'pixel_shuffle')
-        params.update(pnginfo=pnginfo)
-        super().save(fp, format=self.format, **params)
+if PILImage.Image.__name__ != 'EncryptedImage':
+    class EncryptedImage(PILImage.Image):
+        __name__ = "EncryptedImage"
+        def save(self, fp, format=None, **params):
+            if not password:
+                # 如果没有密码，直接保存
+                super().save(fp, format = format, **params)
+                return
+            
+            if 'Encrypt' in self.info and self.info['Encrypt'] == 'pixel_shuffle':
+                super().save(fp, format = format, **params)
+                return
+            
+            encrypt_image(self, get_sha256(password))
+            self.format = PngImagePlugin.PngImageFile.format
+            pnginfo = params.get('pnginfo', PngImagePlugin.PngInfo())
+            pnginfo.add_text('Encrypt', 'pixel_shuffle')
+            params.update(pnginfo=pnginfo)
+            super().save(fp, format=self.format, **params)
 
-        
-def open(fp,*args, **kwargs):
-    image = super_open(fp,*args, **kwargs)
-    if password and image.format.lower() == PngImagePlugin.PngImageFile.format.lower():
-        pnginfo = image.info or PngImagePlugin.PngInfo()
-        if 'Encrypt' in pnginfo and pnginfo["Encrypt"] == 'pixel_shuffle':
-            dencrypt_image(image, get_sha256(password))
-            return image
-    return image
-
+            
+    def open(fp,*args, **kwargs):
+        image = super_open(fp,*args, **kwargs)
+        if password and image.format.lower() == PngImagePlugin.PngImageFile.format.lower():
+            pnginfo = image.info or PngImagePlugin.PngInfo()
+            if 'Encrypt' in pnginfo and pnginfo["Encrypt"] == 'pixel_shuffle':
+                dencrypt_image(image, get_sha256(password))
+                return image
+        return image
+    
+    if password:
+        PILImage.Image = EncryptedImage
+        PILImage.open = open
 
 def on_app_started(demo: Optional[Blocks], app: FastAPI):
     @app.middleware("http")
@@ -50,12 +55,12 @@ def on_app_started(demo: Optional[Blocks], app: FastAPI):
             file_path = endpoint[6:]
             ex = file_path[file_path.rindex('.'):].lower()
             if ex in ['.png','.jpg','.jpeg','.webp','.abcd']:
-                image = super_open(file_path)
+                image = PILImage.open(file_path)
                 if image.format.lower() == PngImagePlugin.PngImageFile.format.lower():
                     pnginfo = image.info or PngImagePlugin.PngInfo()
                     if 'Encrypt' in pnginfo:
                         if pnginfo['Encrypt'] == 'pixel_shuffle':
-                            dencrypt_image(image,get_sha256(password))
+                            # dencrypt_image(image,get_sha256(password))
                             buffered = BytesIO()
                             image.save(buffered, format=PngImagePlugin.PngImageFile.format)
                             decrypted_image_data = buffered.getvalue()
@@ -64,10 +69,7 @@ def on_app_started(demo: Optional[Blocks], app: FastAPI):
         res: Response = await call_next(req)
         return res
 
-
 if password:
-    PILImage.Image = EncryptedImage
-    PILImage.open = open
     script_callbacks.on_app_started(on_app_started)
     print('图片加密已经启动 加密方式 1')
 else:
