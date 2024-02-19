56cf358b1a9e96d9cb6c425d29371f5e85654089
viyiviyi <viyiviyi@outlook.com>
2024-02-19 19:06:33 +0800

    bugfix

--- scripts/encrypt_image.py
+++ scripts/encrypt_image.py
@@ -79,7 +79,7 @@ if PILImage.Image.__name__ != 'EncryptedImage':
             pnginfo.add_text('EncryptPwdSha', get_sha256(f'{get_sha256(password)}Encrypt'))
             for key in (self.info or {}).keys():
                 if self.info[key]:
-                    pnginfo.add_text(key,self.info[key])
+                    pnginfo.add_text(key,str(self.info[key]))
             params.update(pnginfo=pnginfo)
             super().save(fp, format=self.format, **params)
             # 保存到文件后解密内存内的图片，让直接在内存内使用时图片正常
